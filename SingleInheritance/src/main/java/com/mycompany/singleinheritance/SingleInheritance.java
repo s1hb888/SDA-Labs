d267f7e9052f00f3b19648b86c861804da1d1794
Hudda Bibi <97363843+s1hb888@users.noreply.github.com>
2024-10-23 12:37:49 -0700

    single level inheritance

---
+++ SingleInheritance/src/main/java/com/mycompany/singleinheritance/SingleInheritance.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.singleinheritance;
+
+/**
+ *
+ * @author UBH Laptops
+ */
+public class SingleInheritance {
+
+    public static void main(String[] args) {
+    Dog dog=new Dog();
+    dog.eat();
+    dog.bark();
+    }
+}
