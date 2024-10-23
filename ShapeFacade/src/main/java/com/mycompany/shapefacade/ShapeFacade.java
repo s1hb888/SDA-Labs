e57b74885ed5cbc0e2bb2c65a83663fd92bf4d6d
Hudda Bibi <97363843+s1hb888@users.noreply.github.com>
2024-10-23 12:35:46 -0700

    Facade Pattern

---
+++ ShapeFacade/src/main/java/com/mycompany/shapefacade/ShapeFacade.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.shapefacade;
+
+/**
+ *
+ * @author UBH LAPTOPS
+ */
+public class ShapeFacade {
+
+    public static void main(String[] args) {
+       ShapeMaker shapeMaker = new ShapeMaker();
+
+        shapeMaker.drawCircle();
+        shapeMaker.drawRectangle();
+        shapeMaker.drawSquare();
+    }
+}
---
+++ ShapeFacade/src/main/java/com/mycompany/shapefacade/ShapeMaker.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.shapefacade;
+
+/**
+ *
+ * @author UBH LAPTOPS
+ */
+public class ShapeMaker {
+    private Shape circle;
+    private Shape rectangle;
+    private Shape square;
+
+    public ShapeMaker() {
+        circle = new Circle();
+        rectangle = new Rectangle();
+        square = new Square();
+    }
+
+    public void drawCircle() {
+        circle.draw();
+    }
+
+    public void drawRectangle() {
+        rectangle.draw();
+    }
+
+    public void drawSquare() {
+        square.draw();
+    }
+}
