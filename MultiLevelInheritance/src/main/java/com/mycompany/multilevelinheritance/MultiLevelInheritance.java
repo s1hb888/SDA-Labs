5b4e2263d9c0052d5cbefba07b7dc2ad78700ed7
Hudda Bibi <97363843+s1hb888@users.noreply.github.com>
2024-10-23 12:39:35 -0700

    multi level inheritance

---
+++ MultiLevelInheritance/src/main/java/com/mycompany/multilevelinheritance/MultiLevelInheritance.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.multilevelinheritance;
+
+/**
+ *
+ * @author UBH Laptops
+ */
+public class MultiLevelInheritance {
+
+    public static void main(String[] args) {
+    Puppy puppy=new Puppy();
+    puppy.weeps();
+    puppy.bark();
+    puppy.eat();
+    }
+}
---
+++ MultiLevelInheritance/src/main/java/com/mycompany/multilevelinheritance/Puppy.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.multilevelinheritance;
+
+/**
+ *
+ * @author UBH Laptops
+ */
+public class Puppy extends Dog{
+    void weeps(){
+          System.out.println("puppy weeps..");
+}
+}
